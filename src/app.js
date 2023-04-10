a1b939045215aaff68d1aacf4cb2a700ca043137
Luis <l-serrato@live.com>
2023-04-10 18:12:03 -0300

    finish

---
+++ src/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+const teams = [
+  {
+    id: 1,
+    name: 'Clube de Regatas Flamengo',
+    initials: 'CRF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
+app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+app.get('/teams/:id', (req, res) => {
+  const team = teams.find(({ id }) => id === Number(req.params.id));
+  res.status(200).json(team);
+});
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam });
+});
+
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, initials } = req.body;
+
+  const updateTeam = teams.find((team) => team.id === Number(id));
+
+  if (!updateTeam) {
+    res.status(404).json({ message: 'Team not found' });
+  }
+
+  updateTeam.name = name;
+  updateTeam.initials = initials;
+  res.status(200).json({ updateTeam });
+});
+app.delete('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const arrayPosition = teams.findIndex((team) => team.id === Number(id));
+  teams.splice(arrayPosition, 1);
+
+  res.status(200).end();
+});
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,3 @@
+const app = require('./app');
+
+app.listen(1025, () => console.log('server running on port 1025.'));
